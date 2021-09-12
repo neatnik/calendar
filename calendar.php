d3f8e5a36e947d09f8d10a707697900a68b894b3
Adam Newbold <adam@neatnik.net>
2021-09-12 02:12:09 -0400

    + aligned-weekdays parameter

--- calendar.php
+++ calendar.php
@@ -91,6 +91,7 @@
 		-webkit-print-color-adjust: exact;
 	}
 	#info {
+		display: none;
 		font-family: 'Inter', sans-serif;
 		position: absolute;
 		top: 0;
@@ -117,11 +118,14 @@
 	<div id="info">
 	<p>👋 <strong>Hello!</strong> If you print this page, you’ll get a nifty calendar that displays all of the year’s dates on a single page. It will automatically fit on a single sheet of paper of any size. For best results, adjust your print settings to landscape orientation and disable the header and footer.</p>
 	<p>Take in the year all at once. Fold it up and carry it with you. Jot down your notes on it. Plan things out and observe the passage of time. Above all else, be kind to others.</p>
-	<p style="font-size: 80%; color: #999;">Made by <a href="https://neatnik.net/">Neatnik</a> &#183; Shared on <a href="https://github.com/neatnik/calendar">GitHub</a></p>
+	<p style="font-size: 80%; color: #999;">Made by <a href="https://neatnik.net/">Neatnik</a> &#183; Source on <a href="https://github.com/neatnik/calendar">GitHub</a></p>
 	</div>
 	<?php
 	date_default_timezone_set('UTC');
 	$now = isset($_REQUEST['year']) ? strtotime($_REQUEST['year'].'-01-01') : time();
+	$dates = array();
+	$month = 1;
+	$day = 1;
 	echo '<p>'.date('Y', $now).'</p>';
 	echo '<table>';
 	echo '<thead>';
@@ -132,36 +136,111 @@
 	}
 	echo '</tr>';
 	echo '</thead>';
-	$month = 1;
-	$day = 1;
 	echo '<tbody>';
-	// Start the outer loop around 31 days
-	while($day <= 31) {
-		echo '<tr>';
-		// Start the inner loop around 12 months
-		while($month <= 12) {
-			// If we’ve reached a point in the date matrix where the resulting date would be invalid (e.g. February 30th), leave the cell blank
-			if($day > cal_days_in_month(CAL_GREGORIAN, $month, date('Y', $now))) {
-				echo '<td></td>';
-				$month++;
-				continue;
-			}
-			// If the day falls on a weekend, apply a specific class for styles
-			if(DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('N') == 6 || DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('N') == 7) {
-				echo '<td class="weekend">';
+	
+	// Prepare a list of the first weekdays for each month of the year
+	$date = strtotime(date('Y', $now).'-01-01');
+	$first_weekdays = array();
+	
+	for($x = 1; $x <= 12; $x++) {
+		$first_weekdays[$x] = date('N', strtotime(date('Y', $now).'-'.$x.'-01'));
+		$$x = false; // Set a flag for each month so we can track first days below
+	}
+	
+	// Start the loop around 12 months
+	while($month <= 12) {
+		$day = 1;
+		for($x = 1; $x <= 42; $x++) {
+			if(!$$month) {
+				if($first_weekdays[$month] == $x) {
+					$dates[$month][$x] = $day;
+					$day++;
+					$$month = true;
+				}
+				else {
+					$dates[$month][$x] = 0;
+				}
 			}
 			else {
-				echo '<td>';
+				// Ensure that we have a valid date
+				if($day > cal_days_in_month(CAL_GREGORIAN, $month, date('Y', $now))) {
+					$dates[$month][$x] = 0;
+					
+				}
+				else {
+					$dates[$month][$x] = $day;
+				}
+				$day++;
 			}
-			// Display the day number and day of the week
-			echo '<span class="date">'.$day.'</span> <span class="day">'.substr(DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('D'), 0, 1).'</span>';
-			echo '</td>';
-			$month++;
 		}
-		echo '</tr>';
-		$month = 1;
-		$day++;
+		$month++;
 	}
+	
+	// Now produce the table
+	
+	$month = 1;
+	$day = 1;
+	
+	if($_REQUEST['layout'] == 'aligned-weekdays') {
+		// Start the outer loop around 42 days (6 weeks at 7 days each)
+		while($day <= 42) {
+			echo '<tr>';
+			// Start the inner loop around 12 months
+			while($month <= 12) {
+				if($dates[$month][$day] == 0) {
+					echo '<td></td>';
+				}
+				else {
+					
+					$date = date('Y', $now).'-'.str_pad($month, 2, '0', STR_PAD_LEFT).'-'.str_pad($dates[$month][$day], 2, '0', STR_PAD_LEFT);
+					if(date('N', strtotime($date)) == '7') {
+						echo '<td class="weekend">';
+					}
+					else {
+						echo '<td>';
+					}
+					echo $dates[$month][$day];
+					echo '</td>';
+				}
+				$month++;
+			}
+			echo '</tr>';
+			$month = 1;
+			$day++;
+		}
+		
+	}
+	
+	else {
+		// Start the outer loop around 31 days
+		while($day <= 31) {
+			echo '<tr>';
+			// Start the inner loop around 12 months
+			while($month <= 12) {
+				// If we’ve reached a point in the date matrix where the resulting date would be invalid (e.g. February 30th), leave the cell blank
+				if($day > cal_days_in_month(CAL_GREGORIAN, $month, date('Y', $now))) {
+					echo '<td></td>';
+					$month++;
+					continue;
+				}
+				// If the day falls on a weekend, apply a specific class for styles
+				if(DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('N') == 6 || DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('N') == 7) {
+					echo '<td class="weekend">';
+				}
+				else {
+					echo '<td>';
+				}
+				// Display the day number and day of the week
+				echo '<span class="date">'.$day.'</span> <span class="day">'.substr(DateTime::createFromFormat('!Y-m-d', date('Y', $now).'-'.$month.'-'.$day)->format('D'), 0, 1).'</span>';
+				echo '</td>';
+				$month++;
+			}
+			echo '</tr>';
+			$month = 1;
+			$day++;
+		}
+	}
+	
 	?>
 	</tbody>
 	</table>
